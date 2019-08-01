c21a287656f527b11841801d9a46288bd0f7b38f
renard <renard@macbook-pro-de-renard.home>
2019-08-01 08:40:01 +0200

    test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,10 +3,16 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
+    const msg = 'new message';
+    const id = 'message-id';
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { 
+        msg: msg ,
+        id: id
+      }
     })
-    expect(wrapper.text()).toMatch(msg)
+    //expect(wrapper.text()).toMatch(msg);
+    console.log(wrapper.props().id);
+    expect(wrapper.props().id).toBe(id);
   })
 })
