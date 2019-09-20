df03dc40b1958fb871e12fa09e2559ed85e638d6
renard <renard@macbook-pro-de-renard.home>
2019-09-20 07:13:42 +0200

    vue template inspinia

--- src/main.js
+++ src/main.js
@@ -3,7 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import BootstrapVue from 'bootstrap-vue'
+import jquery from 'jquery'
+
+Vue.use(jquery)
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
+
+import '../public/css/style.css'
+import '../public/css/bootstrap.min.css'
+import '../public/css/animate.css'
+import '../public/font-awesome/css/font-awesome.css'
+
 
 new Vue({
   router,
