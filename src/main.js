369241f2c76b5b75d9c3fbb3f0a4c66881c423d7
renard <renard@macbook-pro-de-renard.home>
2019-09-20 21:01:39 +0200

    add jquery in vuejs

--- src/main.js
+++ src/main.js
@@ -6,16 +6,24 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import jquery from 'jquery'
 
+
 Vue.use(jquery)
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
-import '../public/css/style.css'
 import '../public/css/bootstrap.min.css'
-import '../public/css/animate.css'
 import '../public/font-awesome/css/font-awesome.css'
+import '../public/css/animate.css'
+import '../public/css/style.css'
 
+
+
+
+window.$ = require('jquery')
+window.JQuery = require('jquery')
+
+import '../public/js/plugins/pace/pace.min.js'
 
 new Vue({
   router,
